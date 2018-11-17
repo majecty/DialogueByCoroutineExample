b35bb0ff0d444812f6bd886989ae630c5719e444
Juhyeong Park <majecty@gmail.com>
2018-11-17 17:57:44 +0900

    Initial commit

---
+++ Assets/src/Dialogue.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+    [Multiline]
+    [SerializeField]
+    public List<string> text;
+    [SerializeField]
+    Text uiText;
+
+    private State state = State.NotInitialized;
+
+    enum State
+    {
+        NotInitialized,
+        Playing,
+        PlayingSkipping,
+        Completed,
+    }
+
+    IEnumerator Start()
+    {
+        state = State.Playing;
+        for (int i = 0; i < text.Count; i += 1)
+        {
+            yield return PlayLine(text[i]);
+        }
+        state = State.Completed;
+    }
+
+    IEnumerator PlayLine(string text)
+    {
+        for (int i = 0; i < text.Length + 1; i += 1)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if (state == State.PlayingSkipping)
+            {
+                uiText.text = text;
+                state = State.Playing;
+                break;
+            }
+            uiText.text = text.Substring(0, i);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < 25; i += 1)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (state == State.PlayingSkipping)
+            {
+                state = State.Playing;
+                break;
+            }
+        }
+    }
+
+    public void Skip()
+    {
+        state = State.PlayingSkipping;
+    }
+
+    public IEnumerator WaitForComplete()
+    {
+        while (state != State.Completed)
+        {
+            yield return null;
+        }
+    }
+}
